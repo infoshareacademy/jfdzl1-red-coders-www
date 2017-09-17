34e4612b204b64a6cb0f2f0fa0e39af77e530301
HKrzyszczak <h.krzyszczak@electriclight.pl>
2017-09-17 13:18:32 +0200

    add fizzbuzz file

---
+++ js/fizzbuzz.js
@@ -0,0 +1,18 @@
+var firstNumber = 1;
+var lastNumber = 100;
+
+for (var currentNumber = firstNumber; currentNumber <= lastNumber; currentNumber++) {
+  var mod3 = (currentNumber % 3) === 0;
+  var mod5 = (currentNumber % 5) === 0;
+
+  if (mod3 && mod5){
+    console.log("FizzBuzz");
+  } else if (mod3){
+    console.log("Fizz");
+  } else if (mod5){
+    console.log("Buzz");
+  } else {
+    console.log(currentNumber);
+  }
+
+}

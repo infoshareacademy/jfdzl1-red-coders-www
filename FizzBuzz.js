4e0b2eadd55c195408f24b6b1f0f3610d461ae6d
Marcin Domański <marcin@domanski.eu>
2017-09-17 13:26:34 +0200

    Added JS file with FizzBuzz exercise

---
+++ FizzBuzz.js
@@ -0,0 +1,11 @@
+for (var i=1; i<=100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    }
+}

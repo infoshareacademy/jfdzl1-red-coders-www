361bac95ecdd589a06eaa14201070f6f4b5eae4c
Jakub <jakubfedyk@gmail.com>
2017-09-17 13:47:14 +0200

    fix m3 m5

--- FizzBizz.js
+++ FizzBizz.js
@@ -1,17 +1,13 @@
 for (var i = 1; i <= 100; i++) {
-    var m3 = i % 3;
-    var m5 = i % 5;
-
-    if (m3 === 0 && m5 === 0) {
-        console.log('FizzBizz')
-    }
-    else if (m3 === 0) {
-        console.log('Fizz')
-    }
-    else if (m5 === 0) {
-        console.log('Bizz')
-    }
-    else {
-        console.log(i)
+    var m3 = i % 3 === 0;
+    var m5 = i % 5 === 0;
+    if (m3 && m5) {
+        console.log('FizzBizz');
+    } else if (m3) {
+        console.log('Fizz');
+    } else if (m5) {
+        console.log('Bizz');
+    } else {
+        console.log(i);
     }
 }

81c445c4eedc0f743d1a8aa60cb103e6d528ab4d
Jakub <jakubfedyk@gmail.com>
2017-10-01 12:39:37 +0200

    header_fixed_on_top

--- js/header_fixed.js
+++ js/header_fixed.js
@@ -1,7 +1,7 @@
 var elementPosition = $('.header__top').offset();
 $(window).scroll(function(){
     if($(window).scrollTop() > elementPosition.top) {
-        $('.header__top').css('position', 'fixed').css('top', '0');
+        $('.header__top').css('position', 'fixed').css('top', '0').css('z-index', '2' )
     }
     else {
         $('.header__top').css('position','static');

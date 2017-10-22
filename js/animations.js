c65ed3919bed559461bd423acc16cf2273534e11
HKrzyszczak <h.krzyszczak@electriclight.pl>
2017-10-22 13:14:34 +0200

    fix offset in slow scroll

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $('a.internal').click(function(event) {
     event.preventDefault();
+    var offset = $('.header__top').height() + 25;
     $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - 100
+      scrollTop: $($.attr(this, 'href')).offset().top - offset
     }, 800);
   });
 });
